771ac6094970431ceec301fd4cc1fc383157ade6
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-05-08 12:11:35 +0200

    creazione entity e inizio creazione relazioni

--- gestioneeasyvacanza/src/main/java/com/palma/gestioneeasyvacanza/repository/UtenteRepository.java
+++ gestioneeasyvacanza/src/main/java/com/palma/gestioneeasyvacanza/repository/UtenteRepository.java
@@ -3,9 +3,11 @@ package com.palma.gestioneeasyvacanza.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.palma.gestioneeasyvacanza.model.Utente;
+import com.palma.gestioneeasyvacanza.model.Cliente;
+
+
 
 @Repository
-public interface UtenteRepository extends JpaRepository<Utente, Long>{
+public interface UtenteRepository extends JpaRepository<Cliente, Long>{
 
 }
